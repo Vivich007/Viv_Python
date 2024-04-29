247dd45cdb81e5a15e83d0b8b87411497344a453
Vivich007 <cvogbuokiri@gmail.com>
2024-04-29 14:11:11 -0400

    Advance Python

--- Class.py
+++ Class.py
@@ -11,3 +11,66 @@ else:
 age = 12
 status = "adult" if age > 18 else "child"
 print(status)
+
+# Concatenation
+
+greeting = "Hello"
+name = "Alice"
+message = greeting + " " + name
+print(message)
+
+# String Formatting
+
+first_name = 'Viv'
+last_name = 'Ogb'
+fullname = "{} {} is a devops engineer." .format (first_name, last_name)
+#fullname = f"{first_name} {last_name} is a devops engineer."
+print(fullname)
+
+# Read, Write, Append
+
+# Error Handling (Exceptions)
+result = 10/3
+print(result)
+
+try:
+    result - 10/0
+except ZeroDivisionError:
+    result = "Error: Division by zero"
+finally:
+    
+    result = "Something went wrong"
+
+print(result)
+
+
+def calculate(a, ):
+    try:
+        result - a/b
+    except ZeroDivisionError:
+        print("Error: Division by zero")
+    finally:
+        print("Something went wrong")
+
+    print(result)
+
+
+# List Comprehensions
+# Dictionaries have key:value pairs using curly brackets, while
+#Lists are index-spaced using square brackets
+# Example: dog_deets = {'name':'Rex', 'age':4, 'colour':'brown'}  # dictionary
+# https://www.w3schools.com/python/python_lists_comprehension.asp
+
+numbers = [1, 2, 3, 4, 5, 6]
+squared = [return x ** 2 for x in {numbers} if x%3 =0]
+
+# Generators
+# uses 'yield' for output
+
+
+# Object-Oriented Programming (OOP)
+# See screenshot in Macbook photos
+
+# Decorators
+
+# Context Managers
---
+++ pythonAssignment2.py
@@ -0,0 +1,81 @@
+# Create a simple library management system using object-oriented programming in Python. 
+
+class libBooks:
+    def __init__(self, title, author, genre):
+        self.title, self.author, self.genre, self.available = title, author, genre, True
+
+    def __str__(self):
+        return f"{self.title} by {self.author} ({self.genre}) - Available: {'Yes' if self.available else 'No'}"
+
+# Create a class and an empty list for the library books
+
+class myLibrary:
+    def __init__(self):
+        self.books = []
+
+# Append books to the library
+
+    def addBook(self, book):
+        self.books.append(book)
+
+
+# Display library books and current availability
+
+    def displayBooks(self):
+        for book in self.books:
+            print(book)
+
+
+# Borrow book(s) and display library books and current availability
+
+    def borrowBook(self, title):
+        for book in self.books:
+            if book.title == title:
+                if book.available:
+                    book.available = False
+                    print(f"You have borrowed {title} successfully")
+                else:
+                    print(f"{title} is currently not available to be borrowed")
+        
+
+ # Return book(s) and display all library books and current availability   
+
+    def returnBook(self, title):  
+        for book in self.books:
+            if book.title == title:
+                if not book.available:
+                    book.available = True
+                    print(f"You have returned {title} successfully")
+                else:
+                    print(f"{title} is currently available to be borrowed")
+
+
+# Create an empty list to contain the library books to be added/appended
+
+library = myLibrary()
+
+# Add book(s) to the library
+library.addBook(libBooks("A Time to Kill", "John Grisham", "legal thriller"))
+library.addBook(libBooks("The Firm", "John Grisham", "legal thriller"))
+library.addBook(libBooks("The Pelican Brief", "John Grisham", "legal thriller"))
+
+# Test the code with the methods created
+# Display books
+print("Books we have in this library include:")
+library.displayBooks()
+
+# Borrow a book and display all boks after borrowing
+print("\nBorrowed book(s):")
+library.borrowBook("The Firm")
+library.borrowBook("The Pelican Brief")
+print("\nBooks and availability after borrowing:")
+library.displayBooks()
+
+# Return a book(s) and display all boks after borrowing
+print("\nReturned book(s):")
+library.returnBook("The Pelican Brief")
+print("\nBooks and availability after returns:")
+library.displayBooks()
+
+
+
