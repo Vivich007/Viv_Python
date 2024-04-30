eb1d0b48591efddbbc53cfb4820933a62947f795
Vivich007 <cvogbuokiri@gmail.com>
2024-04-29 23:46:55 -0400

    updated files

--- string_operations.sh
+++ string_operations.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
-myVar="Welcome to my Bash Stash!"
-echo $myVar
-echo "${myVar,,}"
+# Task 1: String Manipulation
+
+myVar="Welcome to the best of the best Bash Stash!"
+
+# Change all letters to uppercase
+echo ${myVar^^}
+
+# Count the number of words in the sentence.
+#word_count=$(wc -w <<< "$myVar")
+#echo "Total number of words in the sentence: $word_count"
+echo "Total number of words in the sentence:" $(wc -w <<< "$myVar")
+
+
+# Extract the first word in the sentence.
+first_word=$(echo "$myVar" | awk '{print $1}')
+echo "The first word of the sentence: $first_word"
+
+# Replace all occurrences of the word 'best' with 'coolest'
+echo "$myVar" | sed 's/best/coolest/g'
+
